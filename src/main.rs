10352720ce6ec2c07ccdc77047cef5eb78fd34d9
JohnDoe1112 <johndoe11@posteo.de>
2022-03-24 23:59:21 +0100

    refactor: evaluate game states for splash screen
    item #21

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 //! Basic hello world example.
-use bevy::{prelude::*, input::system::exit_on_esc_system};
+use bevy::{input::system::exit_on_esc_system, prelude::*};
 
 //                                      /;    ;\
 //                                  __  \\____//
@@ -20,12 +20,23 @@ use bevy::{prelude::*, input::system::exit_on_esc_system};
 //     :__\  \   \__/            \  \  :  \   *  8`;'*
 //         `^'                    `^'  `-^-'   \v/ :
 
+// Enum that will be used as a global state for the game
+#[derive(Clone, Eq, PartialEq, Debug, Hash)]
+enum GameState {
+    Splash,
+    Game,
+}
+
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_startup_system(setup)
         .add_system(keyboard_input_system)
         .add_system(exit_on_esc_system)
+        .add_system(state_change_system)
+        .add_state(GameState::Splash)
+        .add_plugin(splash::SplashPlugin)
+        // .add_plugin(game::GamePlugin)
         .run();
 }
 
@@ -37,17 +48,64 @@ struct Velocity(Vec3);
 
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     let texture_handle = asset_server.load("awesome-square.png");
+    commands.spawn_bundle(UiCameraBundle::default());
     commands.spawn_bundle(OrthographicCameraBundle::new_2d());
-    commands.spawn_bundle(SpriteBundle {
-        texture: texture_handle,
-        transform: Transform::from_xyz(300.1,10.0,10.0),
+    commands.spawn_bundle(TextBundle {
+        style: Style {
+            align_self: AlignSelf::FlexEnd,
+            position_type: PositionType::Absolute,
+            position: Rect {
+                bottom: Val::Px(5.0),
+                right: Val::Px(15.0),
+                ..Default::default()
+            },
+            ..Default::default()
+        },
+        // Use the `Text::with_section` constructor
+        text: Text::with_section(
+            // Accepts a `String` or any type that converts into a `String`, such as `&str`
+            "hello\nbevy!",
+            TextStyle {
+                font: asset_server.load("LiberationMono-Regular.ttf"),
+                font_size: 100.0,
+                color: Color::BLACK,
+            },
+            // Note: You can use `Default::default()` in place of the `TextAlignment`
+            TextAlignment {
+                horizontal: HorizontalAlign::Center,
+                ..Default::default()
+            },
+        ),
         ..Default::default()
-    }).insert(Player);
+    });
+    commands
+        .spawn_bundle(SpriteBundle {
+            texture: texture_handle,
+            transform: Transform::from_xyz(300.1, 10.0, 10.0),
+            ..Default::default()
+        })
+        .insert(Player);
 }
 
+fn state_change_system(
+    mut game_state: ResMut<State<GameState>>,
+    keyboard_input: Res<Input<KeyCode>>,
+) {
+    if keyboard_input.just_pressed(KeyCode::U) {
+        if game_state.current() == &GameState::Game {
+            game_state.set(GameState::Splash).unwrap();
+        } else {
+            game_state.set(GameState::Game).unwrap();
+        }
+        dbg!(game_state);
+    }
+}
 
 /// This system moves the square
-fn keyboard_input_system(mut query: Query<&mut Transform, With<Player>>, keyboard_input: Res<Input<KeyCode>>) {
+fn keyboard_input_system(
+    mut query: Query<&mut Transform, With<Player>>,
+    keyboard_input: Res<Input<KeyCode>>,
+) {
     let mut delta = Vec3::default();
     let mut size_delta = Vec3::default();
     let speed = 20.0;
@@ -79,5 +137,60 @@ fn keyboard_input_system(mut query: Query<&mut Transform, With<Player>>, keyboar
     for mut transform in query.iter_mut() {
         transform.translation += delta;
         transform.scale += size_delta;
+    }
+}
+
+/// Generic system that takes a component as a parameter, and will despawn all entities with that component
+fn despawn_all_with_component<T: Component>(to_despawn: Query<Entity, With<T>>, mut commands: Commands) {
+    for entity in to_despawn.iter() {
+        commands.entity(entity).despawn_recursive();
+    }
+}
+
+mod splash {
+    use super::*;
+
+    pub struct SplashPlugin;
+
+    #[derive(Component)]
+    struct OnSplashScreen;
+
+    impl Plugin for SplashPlugin {
+        fn build(&self, app: &mut App) {
+            app.add_system_set(SystemSet::on_enter(GameState::Splash).with_system(splash_setup))
+                .add_system_set(SystemSet::on_exit(GameState::Splash).with_system(despawn_all_with_component::<OnSplashScreen>));
+        }
+    }
+
+    fn splash_setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+        println!("Entered splash screen!");
+        commands.spawn_bundle(TextBundle {
+            style: Style {
+                align_self: AlignSelf::FlexEnd,
+                position_type: PositionType::Absolute,
+                position: Rect {
+                    bottom: Val::Px(5.0),
+                    left: Val::Px(15.0),
+                    ..Default::default()
+                },
+                ..Default::default()
+            },
+            // Use the `Text::with_section` constructor
+            text: Text::with_section(
+                // Accepts a `String` or any type that converts into a `String`, such as `&str`
+                "hello\nbevy!",
+                TextStyle {
+                    font: asset_server.load("LiberationMono-Regular.ttf"),
+                    font_size: 100.0,
+                    color: Color::BLACK,
+                },
+                // Note: You can use `Default::default()` in place of the `TextAlignment`
+                TextAlignment {
+                    horizontal: HorizontalAlign::Center,
+                    ..Default::default()
+                },
+            ),
+            ..Default::default()
+        }).insert(OnSplashScreen);
     }
 }
